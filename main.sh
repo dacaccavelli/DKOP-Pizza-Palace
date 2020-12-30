3275b140251d20401a4182eee61d63750d0e792d
Daniel Caccavelli <dacaccavelli@aumail.averett.edu>
2020-12-30 17:05:20 -0500

    Added a single pizza hash table

--- main.sh
+++ main.sh
@@ -17,9 +17,27 @@ clear
 # Setting up variables for the list of pizzas
 
 pizza_finished=false
+pizzas=()
 
-# Exporting the variables for the other scripts to use
-export pizza_finished
+# 	Creating a single pizza hash table.
+declare -A pizzaA
+pizzaA=(["size"]="medium" ["crust"]="thin" ["toppings"]="1")
+
+
+#Testing
+#pizzas+=($pizzaA)
+
+#for pizza in "${pizzas[@]}"
+#do
+#for choice in "${!pizza[@]}"; do
+#echo "$choice - ${pizza[$choice]}"
+#done
+#done
+
+# Pushpa will need for the pricing
+#for choice in "${!pizzas[@]}"; do
+#echo "$choice - ${pizzas[$choice]}"
+#done
 
 # Welcoming the new customer
 echo "----------------------------------------------------------"
@@ -27,16 +45,25 @@ echo "Welcome to DKOP Pizza Palace! Where dreams become reality!"
 echo "----------------------------------------------------------"
 
 read -p "What is your name? " customername
-echo "Hello $customername."
-export customername
+echo "Hello $customername. Thank you for coming to DKOP Pizza Palace!"
 
+# Listed here are all of the pizzas that have been ordered.
 echo "------------------- Current Order ------------------------"
+echo ""
 
-
-
+echo "----------------------------------------------------------"
 echo "Please selection an option from the list below"
-echo "by using the corresponding number."
+echo "by using the corresponding number:"
+echo "-----------------------------------------------"
 echo "To order a new pizza, enter 1."
 echo "To remove a pizza from the order, enter 2."
 echo "To finish your order, enter 3."
 read -p "Enter your choice..."
+
+
+# Exporting the variables for the other scripts to use
+export pizza_finished
+export customername
+
+# Calling the pizza script for the crust sizes and types.
+
