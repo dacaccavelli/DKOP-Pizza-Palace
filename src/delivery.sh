6aad163aac0981908c9b21087de7bfccf3fec046
Daniel Caccavelli <dacaccavelli@aumail.averett.edu>
2021-01-17 18:36:00 -0500

    Added confirmation for delivery information.

--- src/delivery.sh
+++ src/delivery.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-clear
 
-echo "Hi $customername, we need a little more information for your delivery..."
+info_correct=false
 
 gather-info() {
 read -p "Please enter your last name: " lname
 read -p "Please enter your address: " address
 read -p "Please enter your phone number: " phone
 
-confirmation
 }
 
 confirmation() {
@@ -20,19 +18,28 @@ echo "Address: $address"
 echo "Phone number: $phone"
 read -p "Is the above information correct? (y/n)  " choice
 
-}
-gather-info
-
 case $choice in 
 "yes" | "y") 	echo "Great! Thank you!"
 		touch $customerinfo
 		echo "$customername $lname" >> $customerinfo
 		echo "$address" >> $customerinfo
-		echo "$phone" >> $customerinfo;;
-"no" | "n") 	echo "Let's try that again..."
-		gather-info;;
+		echo "$phone" >> $customerinfo
+		info_correct=true;;
+"no" | "n") 	echo "Let's try that again...";;
 *)		echo "Sorry, I did not understand. Let's try again"
 		confirmation;;
 esac
-
 }
+
+while :; do
+clear
+
+echo "Hi $customername, we need a little more information for your delivery..."
+
+gather-info
+
+confirmation
+
+[ "$info_correct" == "true" ] && break
+
+done
