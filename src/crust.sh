b72b205be5205e919bd4c4d531c0c3eb454f1fd4
Daniel Caccavelli <dacaccavelli@aumail.averett.edu>
2021-01-19 18:58:55 -0500

    Added comments for all except main

--- src/crust.sh
+++ src/crust.sh
@@ -10,13 +10,13 @@
 #---------------------
 # Script Body
 
-# sourcing functions from main.sh without actually running the file
+# Sourcing functions from main.sh without actually running the file.
 source ./src/main.sh --source-only
 
 # Initializing arrays for sizes and crusts and boolean to track
 # when both size and crust type are selected.
 size_arr=( Small Medium Large XLarge)
-crust_arr=( Thin Regular Thick Stuffed)
+crust_arr=( thin regular thick stuffed)
 order_correct=false
 
 size_prompt() {
@@ -25,12 +25,13 @@ size_prompt() {
 	# Text prompt for the user.
 	echo -e "\x1b[33m Please select a size from the list by using the corresponding number."
 
-	# Initializing counter and first option for the choices.
+	# Initializing counter and first option for returning
+	# to the previous menu.
 	counter=1
 	echo -e "\x1b[36m0. Return to main menu"
 
 	for size in ${size_arr[@]}; do
-	# For to which displays the size with the matching choice number.
+	# For loop which displays the size with the matching choice number.
 
 		echo -e "\x1b[36m$counter. $size"
 		((counter++))
@@ -42,23 +43,22 @@ size_prompt() {
 	# and reprompt.
 	read -p "Choose your size: " choice
 	case "$choice" in
-	0) exit;;
+	0) return;;
 	1 | 2 | 3 | 4) size=${size_arr[$choice-1]};;
-	*) 	echo -e "\x1b[31m Answer not recognized."
-		size_prompt
-		;;
+	*) 	clear
+		echo -e "\x1b[31m Answer not recognized."
+		size_prompt;;
 	esac
-} 
+}
 
 crust_prompt() {
 # Function to prompt pizza crust choice and store the result.
 
 	# Initial prompt with user instructions
-	#toilet -f term Please select a crust type from the list by using the corresponding number. --gay
-        echo -e "\x1b[33m Please select a crust type from the list by using the corresponding number."
-        #echo "Enter zero (0) to return to the previous menu." | toilet -f term -F border --gay
+	echo -e "\x1b[33m Please select a crust type from the list by using the corresponding number."
 
-	# Displays the options for the crusts.
+	# Displays the options for the crusts along with the
+	# counter for selecting.
 	echo -e "\x1b[36m0. Return to main menu"
 	counter=1
 	for crust in ${crust_arr[@]}; do
@@ -71,11 +71,10 @@ crust_prompt() {
 	# and reprompt.
 	read -p "Choose your crust: " choice
 	case "$choice" in
-	0) exit;;
+	0) return;;
 	1 | 2 | 3 | 4) crust=${crust_arr[$choice-1]};;
 	*) 	echo -e "\x1b[31m Answer not recognized."
-		crust_prompt
-		;;
+		crust_prompt;;
 	esac
 }
 
@@ -85,7 +84,7 @@ user_prompts() {
 	# Prompts user to choose the size of the pizza
 	# and prints their choice.
 	size_prompt
-	echo -e "\x1b[32m You chose $size"
+	echo -e "\x1b[32m You chose $size size."
 
 	# Prompt user to choose their pizza crust
 	# and prints their choice
@@ -93,8 +92,16 @@ user_prompts() {
 }
 
 confirmation() {
+# Confirms the choice with the user.
+# If yes, changes the order_correct boolean to exit the loop.
+# If no, continues to the next iteration in the loop.
+# If ambiguous, calls confirmation functions again.
 
+	# Reads user input and converts to lowercase.
 	read -p "You chose a $size, $crust crust pizza. Is this correct? (y/n): " choice
+	choice=${choice,,}
+
+	# Switch case to choose proper action based on the input.
 	case $choice in
 	"yes" | "y") 	clear
 			order_correct=true
@@ -111,21 +118,28 @@ confirmation() {
 # Clears the CLI.
 clear
 
+# Main loop
 while :; do
+	# Sourced function from the header which displays the header.
 	header false
 
+	# Prompts the user for both size and crust type.
 	user_prompts
 
+	# Confirms the choice with the user.
 	confirmation
 
+	# If the the result from the confirmation function is yes,
+	# the script will break out of the loop.
 	[ "$order_correct" == "true" ] && break
 
 done
+
 # Creating a file and storing the local variables in there
-# because bash does not have a better way to pass to a
-# parent process.
 # File gets deleted in main.sh after adding the pizza
 # to the running total.
 touch $temppizza
 echo "$size $crust" > $temppizza
+
+# Immediately calls the toppings.sh file to finish the pizza.
 ./src/toppings.sh
--- src/detailed-order.sh
+++ src/detailed-order.sh
@@ -1,37 +1,62 @@
 #!/bin/bash
 
-# Will display the details of each pizza if requested
+#------------------
+# Author: Daniel Caccavelli
+# Date: 1/19/2021
 
-# sourcing functions from main.sh without actually running the file
-source ./src/main.sh --source-only
+# Description: The purpose of this script is to
+# display the details of each pizza if requested.
+# The details include the size, the crust, the
+# names of the chosen toppings, and the price,
+# which gets truncated to fit the screen.
+
+#---------------------------------
+# Script Body
+
 
 display-details() {
-# Displays the size, crust, list of toppings, and price for each pizza
+# Function which displays the size, crust,
+# list of toppings, and price for each pizza.
 
+	# Header which matches the one from the main file.
 	echo "-------------------- DKOP Pizza Palace --------------------" | toilet -f term -F border --gay
         echo -e "\e[1;32m ------------------- Current Order ------------------------ \e[0m"
+
+	# While loop for each line in the file.
         counter=0
         while read line; do
+
+		# Skips the header line
                 if [[ "$counter" == '0' ]]; then
                         (( counter ++ ))
                         continue
                 fi
+
+		# Cuts each line to get the desired portion.
                 size=$(echo $line | cut -f1 -d ' ')
                 crust=$(echo $line | cut -f2 -d ' ')
                 tops=$(echo $line | cut -f3 -d ' ')
                 price=$(echo $line | cut -f4 -d ' ')
 		tops_list=$(echo $line | cut -f2 -d ':')
+
+		# Displays the desired information to the set character amount.
                 echo  -en "\x1b[35m"
 		printf "%-64.64s\n" "$counter. $size, $crust w/ $tops_list"
                 (( counter++ ))
         done < $pizzafile
+
+	# If the counter remains at one, there are no pizzas and
+	# and the default echo will appear.
         if [[ "$counter" == '1' ]]; then
                 echo -e "\e[1;31m The order is currently empty \e[0m"
         fi
-        echo -e "\e[1;32m ---------------------------------------------------------- \e[0m"
 
+        echo -e "\e[1;32m ---------------------------------------------------------- \e[0m"
 }
 
 clear
+
+# Function to display the details and wait for a key
+# press to exit.
 display-details
 read -p "Press any key to return to the previous menu"
